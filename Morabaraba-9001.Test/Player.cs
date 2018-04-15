a04645e022111e31c63f8300f119d43535e2228c
Josh1N <jayngwraai@gmail.com>
2018-04-15 15:43:41 +0200

    Test stamps

---
+++ Morabaraba-9001.Test/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morabaraba
+{
+    public class Player
+    {
+        public int onBoard { get; set; }
+
+        public List<string> positionsHeld { get; set; }
+
+        public string state { get; set; }
+
+        public int unPlaced { get; set; }
+
+        public string name { get; set; }
+
+        public char place { get; set; }
+
+        public List<string[]> playerMills { get; set; }
+    }
+}
